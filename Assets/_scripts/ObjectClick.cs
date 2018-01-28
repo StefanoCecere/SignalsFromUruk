4eed687472f8bc682248876dff203c4db0bc4561
Stefano Cecere <stefano.cecere@krur.com>
2018-01-28 06:18:43 +0100

    init game flow

---
+++ Assets/_scripts/ObjectClick.cs
@@ -0,0 +1,22 @@
+﻿using Fungus;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace SignalsFromUruk
+{
+    public class ObjectClick : MonoBehaviour
+    {
+
+        public Flowchart flowchart;
+
+        public string onMouseDownMessage;
+
+        void OnMouseDown()
+        {
+            if (!string.IsNullOrEmpty(onMouseDownMessage)) {
+                flowchart.SendFungusMessage(onMouseDownMessage);
+            }
+        }
+    }
+}
---
+++ Assets/_scripts/ScenesEnum.cs
@@ -0,0 +1,44 @@
+﻿namespace SignalsFromUruk
+{
+
+    public enum NodesEnums
+    {
+        start,
+        story_problem,
+        place_bathroom,
+        story_misteriousobject,
+        object_box,
+        story_diary,
+        object_tablet_1,
+        object_tablet_2,
+        story_signals,
+        story_zuzu,
+        place_classroom,
+        character_teacher,
+        object_homework,
+        character_teacher_addon,
+        story_gardens,
+        place_garden,
+        character_doctor,
+        object_map,
+        place_river,
+        object_club,
+        object_somar,
+        story_ur,
+        place_ur,
+        character_astronomer,
+        character_astronomer_addon,
+        object_meridian,
+        story_nightsky,
+        object_musicalbox,
+        object_erb,
+        story_medicine,
+        place_temple,
+        character_gilgamesh,
+        character_goddess,
+        character_gilgamesh_addon,
+        story_cure,
+        story_end
+    }
+
+}
